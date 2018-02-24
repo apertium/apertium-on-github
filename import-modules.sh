e9c8ec6d0a8643e693222d7502871d615ef34463
Sushain Cherivirala <sushain@skc.name>
2018-02-23 21:38:48 -0600

    Fixes #17

--- import-modules.sh
+++ import-modules.sh
@@ -3,22 +3,17 @@
 source util.sh
 
 languages=( $(svn ls $SVN_ROOT/languages | grep -E "^apertium-$LANG_RE/$" | cut -d '/' -f 1) )
-for lang in "${languages[@]}"
-do
-    import_create_and_push_repo "$SVN_ROOT/languages/$lang/" $lang '["apertium-languages"]'
-done
+printf "%s\n" "${languages[@]}" | xargs -n 1 -P $MAX_PROCS -I {} bash -c \
+    'import_create_and_push_repo "$SVN_ROOT/languages/{}/" {} '["apertium-languages"]''
 
 modules=( $(svn ls $SVN_ROOT/incubator | grep -E "^apertium-$LANG_RE(-$LANG_RE)?/$" | cut -d '/' -f 1) )
-for module in "${modules[@]}"
-do
-    import_create_and_push_repo "$SVN_ROOT/incubator/$module/" $module '["apertium-incubator"]'
-done
+printf "%s\n" "${modules[@]}" | xargs -n 1 -P $MAX_PROCS -I {} bash -c \
+    'import_create_and_push_repo "$SVN_ROOT/incubator/{}/" {} '["apertium-incubator"]''
 
 PAIR_LOCATIONS=(nursery staging trunk)
 for pair_location in "${PAIR_LOCATIONS[@]}"
 do
     pairs=( $(svn ls $SVN_ROOT/${pair_location} | grep -E "^apertium-$LANG_RE-$LANG_RE/$" | cut -d '/' -f 1) )
-    for pair in "${pairs[@]}"; do
-        import_create_and_push_repo "$SVN_ROOT/${pair_location}/$pair/" $pair "[\"apertium-${pair_location}\"]"
-    done
+    printf "%s\n" "${pairs[@]}" | xargs -n 1 -P $MAX_PROCS -I {} bash -c \
+        'import_create_and_push_repo "$SVN_ROOT/${pair_location}/{}/" {} '["apertium-${pair_location}"]''
 done

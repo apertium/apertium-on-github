2e45ebfbb86f5b0ac5a2d07b7b0ac4b1e01d9f08
Sushain Cherivirala <sushain@skc.name>
2018-02-23 21:23:04 -0600

    Towards #17

--- add-contributors.sh
+++ add-contributors.sh
@@ -11,21 +11,22 @@ do
     payload=$( curl -s -S $GITHUB_API/orgs/$ORG/repos?page=$page -u $USER:$GITHUB_OAUTH_TOKEN )
     new_repos=($(echo $payload | python3 -c "import sys, json; print(' '.join([r['name'] for r in json.load(sys.stdin)]))"))
     set -x
-    repos+=( "${new_repos[@]}" )
+    repos+=( "${new_repos[@]-}" )
     page=$((page + 1))
-    if [ -z "${new_repos}" ]; then
+    if [ -z "${new_repos-}" ]; then
         break
     fi
 done
 
-for repo in "${repos[@]}"
-do
+add_contributors () {
     { set +x; } 2>/dev/null
-    payload=$(curl -s -S $GITHUB_API/repos/$ORG/$repo/contributors -u $USER:$GITHUB_OAUTH_TOKEN)
+    payload=$(curl -s -S $GITHUB_API/repos/$ORG/$1/contributors -u $USER:$GITHUB_OAUTH_TOKEN)
     contributors=($(echo $payload | python3 -c "import sys, json; print(' '.join([r['login'] for r in json.load(sys.stdin)]))"))
     set -x
     for contributor in "${contributors[@]}"
     do
-        curl -s -S -X PUT $GITHUB_API/repos/$ORG/$repo/collaborators/$contributor -u $USER:$GITHUB_OAUTH_TOKEN
+        curl -s -S -X PUT $GITHUB_API/repos/$ORG/$1/collaborators/$contributor -u $USER:$GITHUB_OAUTH_TOKEN
     done
-done
+}
+
+printf "%s\n" "${repos[@]}" | xargs -n 1 -P $MAX_PROCS -I {} bash -c 'add_contributors "{}"'

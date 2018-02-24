a643d79609d5b11b70c380fcacacebc03e9b1df9
Sushain Cherivirala <sushain@skc.name>
2018-02-24 10:24:04 -0600

    Escaping bugfixes

--- add-contributors.sh
+++ add-contributors.sh
@@ -9,7 +9,7 @@ while :
 do
     { set +x; } 2>/dev/null
     payload=$( curl -s -S $GITHUB_API/orgs/$ORG/repos?page=$page -u $USER:$GITHUB_OAUTH_TOKEN )
-    new_repos=($(echo $payload | python3 -c "import sys, json; print(' '.join([r['name'] for r in json.load(sys.stdin)]))"))
+    new_repos=( $(echo $payload | python3 -c "import sys, json; print(' '.join([r['name'] for r in json.load(sys.stdin)]))") )
     set -x
     repos+=( "${new_repos[@]-}" )
     page=$((page + 1))
@@ -21,7 +21,7 @@ done
 add_contributors () {
     { set +x; } 2>/dev/null
     payload=$(curl -s -S $GITHUB_API/repos/$ORG/$1/contributors -u $USER:$GITHUB_OAUTH_TOKEN)
-    contributors=($(echo $payload | python3 -c "import sys, json; print(' '.join([r['login'] for r in json.load(sys.stdin)]))"))
+    contributors=( $(echo $payload | python3 -c "import sys, json; print(' '.join([r['login'] for r in json.load(sys.stdin)]))") )
     set -x
     for contributor in "${contributors[@]}"
     do

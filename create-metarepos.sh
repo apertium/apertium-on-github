afda965d907485f5143f8dcab81edfb407f43456
Sushain Cherivirala <sushain@skc.name>
2018-02-24 21:37:44 -0600

    Make create-metarepos a bit louder

--- create-metarepos.sh
+++ create-metarepos.sh
@@ -6,7 +6,7 @@ create_metarepo () {
     create_repo "apertium-$1"
     set_repo_topics "apertium-$1" '["apertium-all"]'
     init_repo "apertium-$1"
-    ./sync.py sync --repo "apertium-$1"
+    ./sync.py sync --repo "apertium-$1" -v
 }
 
 metarepos=(staging nursery incubator trunk languages tools)
@@ -15,4 +15,4 @@ printf "%s\n" "${metarepos[@]}" | xargs -n 1 -I {} bash -c 'create_metarepo "{}"
 # setup apertium-all
 create_repo "apertium-all"
 init_repo "apertium-all"
-./sync.py sync --repo apertium-all
+./sync.py sync --repo apertium-all -v
